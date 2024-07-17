258b88bc23a9aac91ee9c11d2d224d2546f983ae
KehindeEmmy <kehindeemmy84@gmail.com>
2024-07-17 22:37:49 +0100

    update

--- script.js
+++ script.js
@@ -74,3 +74,35 @@ searchButton.addEventListener('click', function(){
 tClose.addEventListener('click', function() {
   showClass.classList.remove('showsearch')
 })
+
+//Show dpt-menu
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+      dptClass = document.querySelector('.site');
+dptButton.addEventListener('click', function() {
+  dptClass.classList.toggle('showdpt')
+})
+
+// Product image slider
+var productThumb = new Swiper ('.small-image', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    481: {
+      spaceBetween: 32,
+    }
+  }
+});
+var productBig = new Swiper ('.big-image', {
+  loop: true,
+  autoHeight: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: productThumb
+  }
+})
